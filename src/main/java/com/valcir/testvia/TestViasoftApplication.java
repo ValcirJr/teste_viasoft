542a3f320a5a4fcf42462e33dfc779b807c6171b
ValcirJr <balbinottijr@outlook.com>
2021-02-23 14:46:48 -0300

    instanciação categorias

--- src/main/java/com/valcir/testvia/TestViasoftApplication.java
+++ src/main/java/com/valcir/testvia/TestViasoftApplication.java
@@ -1,13 +1,31 @@
 package com.valcir.testvia;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class TestViasoftApplication {
+import com.valcir.testvia.domain.Categoria;
+import com.valcir.testvia.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class TestViasoftApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository catRepo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TestViasoftApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		catRepo.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
