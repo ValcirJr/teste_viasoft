6e2b4a0aa70635b3da966b62447e409bfd92d0f7
ValcirJr <balbinottijr@outlook.com>
2021-02-23 15:01:13 -0300

    produto e associações manytomany

--- src/main/java/com/valcir/testvia/TestViasoftApplication.java
+++ src/main/java/com/valcir/testvia/TestViasoftApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.valcir.testvia.domain.Categoria;
+import com.valcir.testvia.domain.Produto;
 import com.valcir.testvia.repositories.CategoriaRepository;
+import com.valcir.testvia.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class TestViasoftApplication implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaRepository catRepo;
+	
+	@Autowired
+	private ProdutoRepository prodRepo;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(TestViasoftApplication.class, args);
@@ -25,7 +30,22 @@ public class TestViasoftApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		
+		Produto p1 = new Produto(null, "Comuptador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		
 		catRepo.saveAll(Arrays.asList(cat1, cat2));
+		prodRepo.saveAll(Arrays.asList(p1,p2,p3));
+		
 	}
 
 }
