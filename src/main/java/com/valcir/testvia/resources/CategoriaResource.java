b4396b6ba26a745f080fc1a81c7d58e6699d0978
ValcirJr <balbinottijr@outlook.com>
2021-02-23 13:44:23 -0300

    testando primeira classe dominio

--- src/main/java/com/valcir/testvia/resources/CategoriaResource.java
+++ src/main/java/com/valcir/testvia/resources/CategoriaResource.java
@@ -1,16 +1,33 @@
 package com.valcir.testvia.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.valcir.testvia.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
+	
+	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "informatica");
+		Categoria cat2 = new Categoria(1, "escritorio");
+		
+		List<Categoria>	lista = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		
+		return lista;
 	}
 	
 }
